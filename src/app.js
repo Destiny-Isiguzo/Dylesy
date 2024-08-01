213039a9f84a3ded161164084af1705e007ef6ab
Des19 <petrovichalexander411@gmail.com>
2024-08-01 19:00:00 +0300

    "Refactored navigation menu and added hamburger menu for mobile; updated styles in header.css, added scroll reveal animation in app.js"

--- src/app.js
+++ src/app.js
@@ -1,23 +1,40 @@
 /**
- * Add active class and remove menu onclick
-*/
-const navLinks = document.querySelectorAll('.nav-link');
-const nav = document.querySelector('.nav');
-const navMenuToggleBtn = document.querySelector('.nav-menu-toggle-btn')
+ * Handle Navigation
+ */
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const navMenu = document.querySelector('.nav-menu');
+const navMenuLinks = document.querySelectorAll('.nav-menu-link');
 
-function linkAction() {
-   navLinks.forEach((link) => link.classList.remove('nav-link-active'));
-   this.classList.add('nav-link-active');
-   nav.classList.toggle('show-menu');
-   navMenuToggleBtn.classList.toggle('show-menu-icon');
-}
+const toggleMenu = () => {
+   navMenu.classList.toggle('nav-menu-show');
+   hamburgerMenu.classList.toggle('open');
+   animateLinks();
+};
 
-navLinks.forEach((link) => link.addEventListener('click', linkAction));
+const animateLinks = () => {
+   navMenuLinks.forEach((link, index) => {
+      if (navMenu.classList.contains('nav-menu-show')) {
+         link.classList.add('nav-menu-link-in');
+         link.classList.remove('nav-menu-link-out');
+      } else {
+         link.classList.remove('nav-menu-link-in');
+         link.classList.add('nav-menu-link-out');
+      }
+      link.style.animationDelay = `${index * 0.1}s`;
+   });
+};
+
+const linkAction = () => {
+   toggleMenu();
+};
+
+hamburgerMenu.addEventListener('click', toggleMenu);
+navMenuLinks.forEach((link) => link.addEventListener('click', linkAction));
 
 
 /**
  * Dynamically add images after skeleton animations
-*/
+ */
 const imageWrappers = document.querySelectorAll('.hero-image-wrapper');
 
 imageWrappers.forEach((wrapper) => {
@@ -32,3 +49,23 @@ imageWrappers.forEach((wrapper) => {
 
    image.src = imagePath;
 });
+
+
+/**
+ * Scroll reveal animation
+ */
+const sr = ScrollReveal({
+   origin: 'bottom',
+   distance: '60px',
+   duration: 2500,
+   delay: 400,
+   // reset: true
+});
+
+// sr.reveal(`.home__perfil, .contact__mail`, {origin: 'right'});
+// sr.reveal(`.home__name, .home__info, .contact__social, .contact__data`, {origin: 'left'});
+// sr.reveal(`.services .section__title-2, .projects .section__title-1, .about__container, .footer__container`, {
+//    opacity: 0,
+//    distance: '0px',
+// })
+// sr.reveal(`.services__card, .projects__card`, {interval: 110});
